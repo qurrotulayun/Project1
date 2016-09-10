f64b440864206fd5ecc1a8255d200112556be510
Qurrotul A'yun <ayun08101999@gmail.com>
2016-09-11 00:05:24 +0700

    Menambahkan Spinner, Menanmbahkan program agar edittextNama dan RadioButton jika dipilih keluarhasilnya

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6030/project1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl6030/project1/MainActivity.java
@@ -2,12 +2,55 @@ package id.sch.smktelkom_mlg.tugas01.xirpl6030.project1;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText etNama;
+    Button bOk;
+    TextView tvHasil;
+    RadioButton rb1, rb2, rb3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etNama = (EditText) findViewById(R.id.editTextnama);
+        bOk = (Button) findViewById(R.id.buttonOk);
+        tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        rb1 = (RadioButton) findViewById(R.id.radioButton1mlm);
+        rb2 = (RadioButton) findViewById(R.id.radioButton2mlm);
+        rb3 = (RadioButton) findViewById(R.id.radioButton3mlm);
+
+        bOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doProcess();
+
+            }
+        });
+    }
+
+    private void doProcess() {
+
+        String nama = etNama.getText().toString();
+        String hasil = null;
+
+        if (rb1.isChecked()) {
+            hasil = rb1.getText().toString();
+        } else if (rb2.isChecked()) {
+            hasil = rb2.getText().toString();
+        } else if (rb3.isChecked()) {
+            hasil = rb3.getText().toString();
+        }
+
+        tvHasil.setText("Anda bernama " + nama + " \nAnda memesan kamar untuk " + hasil);
+
+
     }
 }
